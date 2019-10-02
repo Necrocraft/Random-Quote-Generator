c6df44497aa988cebc3a739120f712796784649a
Necrocraft <mhdzeefan@gmail.com>
2019-10-02 16:58:08 +0530

    Initial commit with all changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,172 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-useless-constructor */
+import React from "react";
+import "./App.css";
+import $ from "jquery";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const quotes = [
+  {
+    author: "Boss Dog, Bob Ross",
+    content: "There are no mistakes, only happy accidents."
+  },
+  {
+    author: 'Bob "Bob Ross" Ross',
+    content:
+      "Talent is a pursued interest. Anything that you're willing to practice, you can do."
+  },
+  {
+    author: "Bob N. Ross",
+    content: "Alright, let's get crazy."
+  },
+  {
+    author: "Bob Ross",
+    content: "Lets build a happy little cloud."
+  },
+  {
+    author: "Robert Norman Ross",
+    content: "Lets build some happy little trees."
+  },
+  {
+    author: "Bob Ross",
+    content: "And right there you got an almighty cloud."
+  },
+  {
+    author: "Bob Ross the Devil Defeater",
+    content: "Just beat the devil out of it."
+  },
+  {
+    author: "Bob Ross",
+    content: "Let's make a happy little mountain now."
+  },
+  {
+    author: "Bob Ross",
+    content: "There’s nothing wrong with having a tree as a friend."
+  },
+  {
+    author: "Bob Ross",
+    content: "Alizarin Crimson"
+  },
+  {
+    author: "Bob Ross",
+    content: "Cadmium Yellow"
+  },
+  {
+    author: "Bob Ross",
+    content: "Phthalo Blue"
+  },
+  {
+    author: "Bob Ross",
+    content: "Van Dyke Brown"
+  },
+  {
+    author: "Bob Ross",
+    content: "Yellow Ochre"
+  }
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: "",
+      author: ""
+    };
+
+    this.newQuote = this.newQuote.bind(this);
+  }
+
+  newQuote() {
+    let number = Math.floor(Math.random() * 14);
+
+    let quote = quotes[number].content;
+
+    this.setState({
+      content: quote
+    });
+
+    let author = quotes[number].author;
+    this.setState({
+      author: author
+    });
+
+    $(".quote-text").addClass("button-anim");
+    $(".author-text").addClass("button-anim");
+    setTimeout(function() {
+      $(".quote-text").removeClass("button-anim");
+      $(".author-text").removeClass("button-anim");
+    }, 1500);
+  }
+
+  componentDidMount() {
+    this.newQuote();
+  }
+
+  render() {
+    return (
+      <div id="quote-box" className="center-div">
+        {" "}
+        Random Quote Machine <hr />
+        <Text quote={this.state.content} author={this.state.author} />{" "}
+        <Buttons
+          handleNewQuote={this.newQuote}
+          quote={this.state.content}
+          author={this.state.author}
+        />{" "}
+      </div>
+    );
+  }
+}
+
+class Text extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1 id="text" class="quote-text button-anim">
+          {" "}
+          "{this.props.quote}"{" "}
+        </h1>{" "}
+        <h2 id="author" class="author-text button-anim">
+          {" "}
+          - {this.props.author}{" "}
+        </h2>
+      </div>
+    );
+  }
+}
+
+class Buttons extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="buttons">
+        <button
+          type="button"
+          id="new-quote"
+          className="btn btn-success"
+          onClick={this.props.handleNewQuote}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {" "}
+          New Quote{" "}
+        </button>{" "}
+        <button className="btn btn-info" id="tweet-quote">
+          {" "}
+          <a
+            target="_blank"
+            rel="noreferrer noopener"
+            href={`https://twitter.com/intent/tweet/?text=${this.props.quote} - ${this.props.author}`}
+          >
+            {" "}
+          </a>
+          Tweet Quote{" "}
+        </button>{" "}
+      </div>
+    );
+  }
 }
 
 export default App;
